7d7fd28fa1947cad4ca99d2217e2575ff503f38b
jaydenbrant <125827406+jaydenbrant@users.noreply.github.com>
2023-05-14 16:29:50 -0700

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,7 +6,7 @@ import { SignUpForm } from '@/components/SignUpForm'
 
 function CalendarIcon(props) {
   return (
-    <svg viewBox="0 0 8 8" aria-hidden="true" fill="currentColor" {...props}>
+    <svg viewBox="0 0 32 32" aria-hidden="true" fill="currentColor" {...props}>
       <path d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z" />
     </svg>
   )
@@ -14,7 +14,7 @@ function CalendarIcon(props) {
 
 function PhoneIcon(props) {
   return (
-    <svg viewBox="0 0 12 12" aria-hidden="true" fill="currentColor" {...props}>
+    <svg viewBox="0 0 24 24" aria-hidden="true" fill="currentColor" {...props}>
       <path d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" />
     </svg>
   )
