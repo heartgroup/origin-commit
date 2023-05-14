43c58d34c67eabd80dc5e59f8516b1753b536e9c
jaydenbrant <125827406+jaydenbrant@users.noreply.github.com>
2023-05-14 16:36:33 -0700

    Update Intro.jsx

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -49,7 +49,7 @@ export function Intro() {
         </Link>
       </div>
       <h1 className="mt-14 font-display text-4xl/tight font-semibold text-white">
-        ORIGIN / {' '}
+        ORIGIN <span className="font-normal">/</span> {' '}
         <span className="text-sky-300 font-light">Intelligence</span>
       </h1>
       <p className="mt-4 text-sm/6 text-gray-300">
