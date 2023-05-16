983864e5e22308d134c26f67579c9d0882220949
jaydenbrant <125827406+jaydenbrant@users.noreply.github.com>
2023-05-15 18:22:42 -0700

    Update ModalWrapper.jsx

--- src/components/ModalWrapper.jsx
+++ src/components/ModalWrapper.jsx
@@ -11,10 +11,10 @@ const ModalWrapper = ({ open, onClose, icon: Icon, title, children }) => {
       <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={onClose}>
         <Transition.Child
           as={Fragment}
-          enter="ease-out duration-300"
+          enter="ease-out duration-1000"
           enterFrom="opacity-0"
           enterTo="opacity-100"
-          leave="ease-in duration-200"
+          leave="ease-in duration-1000"
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
