e25cf8dd3ff102d9b680cfdd33e61d71265f3c58
IronHeart <ironheart0225@gmail.com>
2023-05-16 06:07:48 +0300

    implement email sending functionality, relayout contact us

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -2,28 +2,28 @@ const ContactUs = () => {
   return (
     <div className="typography text-center">
       <div className="w-fit mx-auto">
-        <h2 className="w-fit uppercase">Los Angeles</h2>
-        <div className="w-full h-[1px] bg-[#111827]" />
+        <h2 className="w-fit uppercase !mt-4">Los Angeles</h2>
+        <div className="w-full h-[1px] bg-[#111827] !mt-2" />
       </div>
-      <p>
+      <p className="!mt-4">
         515 S. Flower Street 18th Floor<br />
         Los Angeles, CA 90071<br />
         (213) 314-5229
       </p>
       <div className="w-fit mx-auto">
-        <h2 className="w-fit uppercase">New York</h2>
-        <div className="w-full h-[1px] bg-[#111827]" />
+        <h2 className="w-fit uppercase !mt-4">New York</h2>
+        <div className="w-full h-[1px] bg-[#111827] !mt-2" />
       </div>
-      <p>
+      <p className="!mt-4">
         287 Park Avenue South 2nd Floor<br />
         New York, NY 10010<br />
         (212) 860-0098
       </p>
       <div className="w-fit mx-auto">
-        <h2 className="w-fit uppercase">Washington DC</h2>
-        <div className="w-full h-[1px] bg-[#111827]" />
+        <h2 className="w-fit uppercase !mt-4">Washington DC</h2>
+        <div className="w-full h-[1px] bg-[#111827] !mt-2" />
       </div>
-      <p>
+      <p className="!mt-4">
         1441 L Street NW 10th Floor<br />
         Washington, DC 20005<br />
         (202) 558-4090
--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,12 +1,51 @@
-import { useId } from 'react'
-
+import { useId, useState } from 'react'
+import emailjs from 'emailjs-com'
 import { Button } from '@/components/Button'
 
 export function SignUpForm() {
+  const [emailAddress, setEmailAddress] = useState('');
+  const [isSent, setIsSent] = useState(false);
+  const [errorText, setErrorText] = useState('');
   let id = useId()
 
+  const validateEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  }
+
+  const handleChangeEmail = (e) => {
+    setEmailAddress(e.target.value);
+    if (!!e.target.value && !validateEmail(e.target.value)) {
+      setErrorText('Invalid email address');
+    } else {
+      setErrorText(''); 
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!validateEmail(emailAddress)) {
+      return;
+    }
+  
+    const serviceID = 'service_6ureu2q';
+    const templateID = 'template_0jjieaz';
+    const userID = 'PcVIawm6HMW7VKYcV';
+  
+    emailjs
+      .send(serviceID, templateID, { emailAddress }, userID)
+      .then((response) => {
+        setIsSent(true);
+        setEmailAddress('A representative will contact you.');
+        console.log('Email sent successfully!', response);
+      })
+      .catch((error) => {
+        setErrorText('Error sending email');
+      });
+  }
+
   return (
-    <form className="relative isolate mt-8 flex items-center pr-1">
+    <form onSubmit={handleSubmit} className="relative isolate mt-8 flex items-center pr-1">
       <label htmlFor={id} className="sr-only">
         Email address
       </label>
@@ -18,9 +57,19 @@ export function SignUpForm() {
         id={id}
         placeholder="Email address"
         className="peer w-0 flex-auto bg-transparent px-4 py-2.5 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-[0.8125rem]/6"
+        disabled={isSent}
+        value={emailAddress}
+        onChange={handleChangeEmail}
       />
-      <Button type="submit" arrow>
-        Get access
+      {!!errorText && (
+        <span className="absolute left-2 bottom-[-28px] text-sm text-red-800">{errorText}</span>
+      )}
+      <Button
+        type="submit"
+        arrow={!isSent}
+        disabled={isSent}
+      >
+        {isSent ? "Thank you" : "Get access"}
       </Button>
       <div className="absolute inset-0 -z-10 rounded-lg transition peer-focus:ring-4 peer-focus:ring-sky-300/15" />
       <div className="absolute inset-0 -z-10 rounded-lg bg-white/2.5 ring-1 ring-white/15 transition peer-focus:ring-sky-300" />
